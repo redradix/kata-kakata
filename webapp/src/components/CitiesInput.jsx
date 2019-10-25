0e8b6047c69bf780b0098cd06cf1aac88e4e6f89
Álvaro Cuesta <alvaro@redradix.com>
2019-10-25 17:29:05 +0200

    Only fetch cities on input blur

--- webapp/src/components/CitiesInput.jsx
+++ webapp/src/components/CitiesInput.jsx
@@ -1,22 +1,16 @@
 import React, { useState } from 'react'
 
 const CitiesInput = ({ onChange }) => {
-  const [input, setInput] = useState('')
-
   const handleChange = ({ target }) => {
-    const text = target.value
-
-    setInput(text)
-    onChange(text.split(','))
+    onChange(target.value.split(','))
   }
 
   return (
     <div>
       <input
         type="text"
-        value={input}
         placeholder="comma separated cities"
-        onChange={handleChange}
+        onBlur={handleChange}
       />
     </div>
   )
