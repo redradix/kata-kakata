8b4a5ccd922bc3faee320d738a5708702a6b0daf
Kevin Perrée <kevin@redradix.com>
2019-10-25 17:29:32 +0200

    table works but no check on timestamp

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -3,19 +3,28 @@ import Table from './Table'
 import * as R from 'ramda'
 
 function App({ attributes }) {
-  const [value, setValue] = useState('madrid,paris')
+  const [value, setValue] = useState(null)
+  const [data, setData] = useState(null)
+
+  const start = () => {
+    getFormData();
+    let timer = setInterval(() => getFormData(), 5000);
+  }
 
   const getFormData = () => {
     const dataToApi = value.split(',')
     const citiesWithAttrs = R.xprod(dataToApi, attributes)
-
     const urlParams = citiesWithAttrs.map((cityWithAttrs) => {
       return cityWithAttrs.join('.')
     }).join(',')
 
     fetch(`http://localhost:3001/${urlParams}`)
-      .then((response) => {
-        console.log(response.json())
+      .then(response => {
+        return response.json()
+      })
+      .then(data => {
+        let cityArray = R.values(R.groupBy(R.prop('city'),data))
+        setData(cityArray)
       })
   }
 
@@ -23,10 +32,10 @@ function App({ attributes }) {
     <div className="App">
       <div>
         <input type='text' placeholder='comma separated cities' onChange={event => setValue(event.target.value)} />
-        <button onClick={getFormData}></button>
+        <button onClick={start}>TEST</button>
       </div>
       <div id='data-section' className='data'>
-        <Table data={'hola'} attributes={attributes}></Table>
+        <Table data={data} attributes={attributes}></Table>
       </div>
     </div>
   )
--- webapp/src/Table.js
+++ webapp/src/Table.js
@@ -1,6 +1,9 @@
 import React from 'react'
+import * as R from 'ramda'
 
-function App({ attributes }) {
+function App({ data, attributes }) {
+
+  data = data?data:[]
 
   const heads = [...['city', 'date'], ...attributes]
 
@@ -11,6 +14,22 @@ function App({ attributes }) {
           {heads.map((head) => <th key={head}>{head}</th>)}
         </tr>
       </thead>
+      <tbody> 
+        {
+          data.map((city, index)=> { 
+            var getTimestamps = R.pluck('timestamp')
+            const timestamp = new Date(R.max(...getTimestamps(city))).toISOString()
+
+            return <tr key={index}>
+              <td>{city[index]['city']}</td>
+              <td>{timestamp}</td>
+              {
+                city.map((attr)=>
+                  <td key={attr.id}>{attr.value.toFixed(5)}</td>
+                )}
+            </tr>
+          })}
+      </tbody>
     </table>
   )
 }
