1a8e991a072d4991d236163d5dfcd8c15bea5658
Álvaro Cuesta <alvaro@redradix.com>
2019-10-25 16:24:02 +0200

    Fetch cities parameters when they change

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -1,9 +1,31 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import * as R from 'ramda'
 import CitiesInput from './components/CitiesInput'
 import CitiesTable from './components/CitiesTable'
 
+const API_HOST = 'http://localhost:3001'
+
+const getAPIParam = R.pipe(
+  R.xprod,
+  R.map(R.join('.')),
+  R.join(','),
+)
+
+const getEndpoint = (cities, attributes) =>
+  `${API_HOST}/${getAPIParam(cities, attributes)}`
+
 function App({ attributes }) {
   const [cities, setCities] = useState([])
+
+  useEffect(() => {
+    if (!cities) {
+      return
+    }
+
+    fetch(getEndpoint(cities, attributes))
+      .then(x => x.json())
+      .then(console.log)
+  }, [cities])
 
   return (
     <div className="App">
