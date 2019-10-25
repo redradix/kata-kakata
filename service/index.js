a73bba22939e03cecad7b923136bbde4995bc539
Victor Miguez <victor@redradix.com>
2019-10-25 15:30:17 +0200

    changes service port

--- service/index.js
+++ service/index.js
@@ -7,7 +7,7 @@ const makeWebapp = require('./webapp')
 const repo = makeRepo()
 makeWebapp(app, repo)
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3001
 server.listen(PORT, () => {
   console.log('listening on *: ' + PORT)
 })
